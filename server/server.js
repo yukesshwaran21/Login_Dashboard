c10962fc75975f579256ca2eb9513a61d4533e0f
yukesshwaran21 <yukesshwaran6@gmail.com>
2026-08-06 17:24:20 +0530

    Phase 1 completed

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server is Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
