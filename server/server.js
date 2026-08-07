96766a2710a943bfef025a63546faa0c72193134
yukesshwaran21 <yukesshwaran6@gmail.com>
2026-08-07 13:50:42 +0530

    cors issue fixed

--- server/server.js
+++ server/server.js
@@ -14,9 +14,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: [
-            "http://localhost:5173"
-        ],
+        origin: process.env.CLIENT_URL,
         credentials: true
     })
 );
